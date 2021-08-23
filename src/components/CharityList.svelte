70621e107d60189ef764f00f39f8091057fa6d6e
Ikhsan Nur <ikhsan15nur@gmail.com>
2021-08-23 17:09:16 +0700

    mengirimkan data antar komponen

---
+++ src/components/CharityList.svelte
@@ -0,0 +1,13 @@
+<script>
+	// export menangkap data carities
+	export let charities = ["default charity 1"];
+</script>
+
+<div>
+	<h2>Daftar Charity</h2>
+	<pre>{charities}</pre>
+	<ul>
+		<li>Charity 1</li>
+		<li>Charity 2</li>
+	</ul>
+</div>
---
+++ src/pages/Home.svelte
@@ -0,0 +1,21 @@
+<script>
+	import CharityList from "../components/CharityList.svelte";
+
+	let title = "Charity";
+
+	let charities = ["Charity 1", "Charity 2", "Charity 3"];
+</script>
+
+<div>
+	<h1>{title}</h1>
+	<!-- mengirimkan data charities -->
+	<!-- <CharityList charities = {charities} /> -->
+	<CharityList {charities} />
+</div>
+
+<style>
+	h1 {
+		font-size: 3em;
+		color: #e2071c;
+	}
+</style>
