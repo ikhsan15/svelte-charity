df3f8aa41b8d51aef36799db90ba8b40f69b967a
Ikhsan Nur <ikhsan15nur@gmail.com>
2021-08-24 16:29:35 +0700

    HTML Template Ke Komponen Svelte

--- src/data/charities.js
+++ src/data/charities.js
@@ -4,11 +4,12 @@ module.exports = {
       id: 1,
       title: 'Firts Charity Project',
       category: 'Money',
-      thumbnail: 'https://live.staticflickr.com/7721/17102022497_84b7c9c342_w.jpg',
-      pluged: 0,
+      // thumbnail: 'https://live.staticflickr.com/7721/17102022497_84b7c9c342_w.jpg',
+      thumbnail: 'https://live.staticflickr.com/2569/4013944382_469787def0_z.jpg',
+      pledged: 99000,
       target: 100000,
       date_end: +new Date('31 August 2021'),
-      // profile_photo: 'https://live.staticflickr.com/4027/4357722810_7136f4a9e9_s.jpg',
+      profile_photo: 'https://live.staticflickr.com/4027/4357722810_7136f4a9e9_s.jpg',
       profile_name: 'Viroide Bueno',
       no_pledges: 0
     }
--- src/pages/Home.svelte
+++ src/pages/Home.svelte
@@ -1,22 +1,17 @@
 <script>
 	import CharityList from "../components/CharityList.svelte";
+	import Header from "../components/Header.svelte";
+	import Welcome from "../components/Welcome.svelte";
+	import Promo from "../components/Promo.svelte";
+	import Footer from "../components/Footer.svelte";
+
 	import { charities } from "../data/charities.js";
 
 	let title = "Charity";
-	// let charities = ["Charity 1", "Charity 2", "Charity 3", "Charity 4"];
 </script>
 
-<div>
-	<h1>{title}</h1>
-	<!-- mengirimkan data charities -->
-	<!-- <CharityList charities = {charities} /> -->
-	<CharityList {charities} />
-	<!-- <CharityList /> -->
-</div>
-
-<style>
-	h1 {
-		font-size: 3em;
-		color: #e2071c;
-	}
-</style>
+<Header />
+<Welcome />
+<CharityList {charities} />
+<Promo />
+<Footer />
